9f8169d8a80db725cab93280197b2f226c4dcc08
unknown <mark adonteng>
2024-11-11 14:26:57 +0000

    Changed webApp name

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       includeAssets: ['TaylorEduExpress.jpg', 'robots.txt', 'apple-touch-icon.png', 'TaylorEduExpress.jpg'],
       manifest: {
         name: 'Taylors Educational Express',
-        short_name: 'T.E.E',
+        short_name: 'Taylors Educational Express',
         description: 'Taylor E-learning platform',
         theme_color: '#ffffff',
         icons: [
